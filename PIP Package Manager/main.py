a4d6282a5fdb039b58192c5997bbbbaab6d28aaf
Reza Malik <rezamalikdrive@gmail.com>
2023-11-10 17:10:28 +0700

    string color and emoji

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,12 @@
+from stringcolor import *
+from emoji import *
+
+print(cs("Reza Malik","blue"))
+print(bold("Selamat Datang"))
+print(cs("Malik", "gold").underline())
+print(cs("Nama saya", "blue", "white"))
+
+print(emojize(":winking_face:"))
+print(emojize(":upside_down_face:"))
+print(emojize(":smiling_face_with_halo:"))
+print(emojize(":thumbs_up:"))
